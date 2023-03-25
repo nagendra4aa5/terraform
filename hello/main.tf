5bd26becf71704fa9657e3464ea2975eed956953
nagendra4aa5 <nagendra4aa5@gmail.com>
2023-03-25 16:23:00 +0530

    Add

---
+++ hello/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "nnp4a5-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
